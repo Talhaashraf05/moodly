af56189d7acf5d67074c556396822c3473ffe5be
talhaashraf05 <talha476422@gmail.com>
2024-08-21 14:52:43 +0500

    Initial Nest.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,27 @@
+import Main from "@/components/Main";
+import Login from "@/components/Login";
+
+export const metadata = {
+    title: "Moodly ⋅ Dashboard",
+};
+
 export  default function DashboardPage() {
+
+    const isAuthenticated = false;
+
+    let children = (
+        <Login />
+    )
+
+    if (isAuthenticated) {
+        children = (
+            <div>Dashboard</div>
+        )
+    }
+
     return (
-        <>
-            <h1>
-                Hello world
-            </h1>
-        </>
+        <Main>
+            {children}
+        </Main>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Main from "@/components/Main";
+import Hero from "@/components/Hero";
 
 export default function HomePage() {
   return (
-    <main >
-
-    </main>
+    <Main >
+      <Hero />
+    </Main>
   );
 }
