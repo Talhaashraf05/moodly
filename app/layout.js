3db88c29d23b6d09b554ef7826b8f50a240f18f3
talhaashraf05 <talha476422@gmail.com>
2024-08-21 15:53:31 +0500

    Added Header and Footer Fixed Tailwind

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,34 @@
-import { Inter } from "next/font/google";
+import {Fugaz_One, Open_Sans} from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const opensans = Open_Sans({ subsets: ["latin"] });
+const fugaz = Fugaz_One({ subsets: ["latin"] , weight: ['400'] });
+
 
 export const metadata = {
-  title: "Moodly",
-  description: "Track your daily mood every day with Moodly",
+    title: "Moodly",
+    description: "Track your daily mood every day with Moodly",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-      <header>header</header>
-      {children}
-      <footer>footer</footer>
-      </body>
-    </html>
-  );
+    const header = (
+        <header className="p-4 sm:p-8 flex items-center justify-between">
+            <h1 className={ 'text-base sm:text-lg textGradient ' + fugaz.className}>Moodly</h1>
+        </header>
+    )
+    const footer = (
+        <footer className="p-4 sm:p-8 grid place-items-center">
+            <p className={'text-indigo-500 ' + fugaz.className}>Created with ❤️</p>
+        </footer>
+    )
+
+    return (
+        <html lang="en">
+        <body className={'w-full max-w-[1000px] mx-auto text-sm sm:text-base min-h-screen flex flex-col  text-slate-800 ' + opensans.className}>
+            {header}
+            {children}
+            {footer}
+        </body>
+        </html>
+    );
 }
